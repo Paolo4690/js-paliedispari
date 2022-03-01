06929ae9ea8fd1eb584b630ae48e96a532e84df2
Paolo4690 <paolo.calafiore.90@gmail.com>
2022-03-01 18:01:52 +0100

    add controlli

--- js/script.js
+++ js/script.js
@@ -51,11 +51,6 @@ function control1Palindromo(stringa) {
 };
 
 
-
-
-
-
-
 /*Pari e Dispari
 L’utente sceglie pari o dispari e inserisce un numero da 1 a 5.
 Generiamo un numero random (sempre da 1 a 5) per il computer (usando una funzione).
@@ -64,42 +59,54 @@ Stabiliamo se la somma dei due numeri è pari o dispari (usando una funzione)
 Dichiariamo chi ha vinto.*/
 
 // ELEMENTI HTML // ELEMENTI HTML // ELEMENTI HTML // ELEMENTI HTML 
+const numero = document.querySelector('#number-user')
 const btnPari = document.querySelector('#btn-pari');
 const btnDispari = document.querySelector('#btn-dispari');
 const resultPariDispari = document.querySelector('#result-pari-dispari');
 
 // CLICK PARI // CLICK PARI // CLICK PARI // CLICK PARI // CLICK PARI 
 btnPari.addEventListener('click', function() {
-    let numeroUser = parseInt(prompt('inserisci un numero da 1 a 5'));
-    let numeroPc = random(1, 5);
-    let sommaNum = somma(numeroUser, numeroPc)
-    let numeroPari = pari(sommaNum)
-    
-    let frase =`Tu hai scelto ${numeroUser},<br>Il Pc ha scelto ${numeroPc},<br>
-    La somma dei numeri è ${sommaNum},<br>`;
-
-    if (numeroPari == true) {
-        resultPariDispari.innerHTML = `${frase} la somma è pari! HAI VINTO`
+    let numeroUser = parseInt(numero.value);
+    if (numeroUser > 5 || numeroUser <= 0) {
+        resultPariDispari.innerHTML = 'Inserisci un numero compreso tra 1 e 5'
     } else {
-        resultPariDispari.innerHTML = `${frase} la somma è dispari! HAI PERSO`
+        let numeroPc = random(1, 5);
+        let sommaNum = somma(numeroUser, numeroPc)
+        let numeroPari = pari(sommaNum)
+        
+        let frase =`Tu hai scelto ${numeroUser},<br>Il Pc ha scelto ${numeroPc},<br>
+        La somma dei numeri è ${sommaNum},<br>`;
+    
+        if (numeroPari == true) {
+            resultPariDispari.innerHTML = `${frase} la somma è pari! HAI VINTO`
+        } else {
+            resultPariDispari.innerHTML = `${frase} la somma è dispari! HAI PERSO`
+        }
     }
+
+
 });
 
 // CLICK DISPARI // CLICK DISPARI// CLICK DISPARI// CLICK DISPARI
 btnDispari.addEventListener('click', function() {
-    numeroUser = parseInt(prompt('inserisci un numero da 1 a 5'));
-    numeroPc = random(1, 5);
-    sommaNum = somma(numeroUser, numeroPc)
-    numeroPari = pari(sommaNum)
-
-    let frase =`Tu hai scelto ${numeroUser},<br>Il Pc ha scelto ${numeroPc},<br>
-    La somma dei numeri è ${sommaNum},<br>`;
-
-    if (numeroPari == false) {
-        resultPariDispari.innerHTML = `${frase} la somma è dispari! HAI VINTO`
+    let numeroUser = parseInt(numero.value);
+    if (numeroUser > 5 || numeroUser <= 0) {
+        resultPariDispari.innerHTML = 'Inserisci un numero compreso tra 1 e 5'
     } else {
-        resultPariDispari.innerHTML = `${frase} la somma è pari! HAI PERSO`
+        let numeroPc = random(1, 5);
+        let sommaNum = somma(numeroUser, numeroPc)
+        let numeroPari = pari(sommaNum)
+
+        let frase =`Tu hai scelto ${numeroUser},<br>Il Pc ha scelto ${numeroPc},<br>
+        La somma dei numeri è ${sommaNum},<br>`;
+
+        if (numeroPari == false) {
+            resultPariDispari.innerHTML = `${frase} la somma è dispari! HAI VINTO`
+        } else {
+            resultPariDispari.innerHTML = `${frase} la somma è pari! HAI PERSO`
+        }
     }
+
 });
 
 // FUNZIONI GENERICHE // FUNZIONI GENERICHE // FUNZIONI GENERICHE // FUNZIONI GENERICHE 
